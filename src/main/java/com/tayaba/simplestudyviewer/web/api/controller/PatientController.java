c3668982b59a2307b0bf5980978053c394dabb1d
ummeTayaba <tayebasadna8@gmail.com>
2021-05-31 19:10:05 +0600

    added patient's crud operation in controller

--- src/main/java/com/tayaba/simplestudyviewer/web/api/controller/PatientController.java
+++ src/main/java/com/tayaba/simplestudyviewer/web/api/controller/PatientController.java
@@ -1,4 +1,51 @@
 package com.tayaba.simplestudyviewer.web.api.controller;
 
+import com.tayaba.simplestudyviewer.models.Patient;
+import com.tayaba.simplestudyviewer.web.api.repositories.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
 public class PatientController {
+
+    @Autowired
+    private PatientRepository patientRepository;
+
+    @RequestMapping(value = "/patients", method = RequestMethod.GET)
+    public Iterable<Patient> getPatients() {
+
+        return patientRepository.findAll();
+    }
+
+    @RequestMapping(value = "/patients", method = RequestMethod.POST)
+    public Patient addPatient(Patient patient) {
+
+        return patientRepository.save(patient);
+    }
+
+    @RequestMapping(value = "/patients", method = RequestMethod.PATCH)
+    public Patient updatePatient(Patient patient) {
+        return patientRepository.save(patient);
+    }
+
+    @RequestMapping(value = "/patients/{id}", method = RequestMethod.DELETE)
+    public Patient removePatient(@PathVariable("id") Long id) {
+        Optional<Patient> patient = patientRepository.findById(id);
+
+        if (patient.isPresent()) {
+
+            patientRepository.deleteById(id);
+            return patient.get();
+        }
+
+        throw new EmptyResultDataAccessException(1);
+    }
+
+
 }
